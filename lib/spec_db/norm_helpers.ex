ce16a37c735bb563a87dfd768a072ff1f1f4cabc
aaron <aaron@pop-os.localdomain>
2022-03-29 06:08:11 -0700

    Fix up Norm specs

--- lib/spec_db/norm_helpers.ex
+++ lib/spec_db/norm_helpers.ex
@@ -25,16 +25,13 @@ defmodule Sorcery.SpecDb.NormHelpers do
         %{one_of: li} -> one_of(Enum.map(li, fn i ->
           spec(fn value -> i == value end)
         end))
-        %{t: :list, coll_of: t, length: l} -> coll_of(get_t_spec(t), min_count: l, max_count: l)
-        %{t: :list, coll_of: t} -> coll_of(get_t_spec(t))
-        %{t: :id} -> spec(is_integer())
-        %{t: :integer} -> spec(is_integer())
-        %{t: :string} -> spec(is_binary())
-        %{t: :binary} -> spec(is_binary())
-        %{t: :float} -> spec(is_float())
-        %{t: :bool_array} -> coll_of(spec(is_boolean()))
-        %{t: :integer_array} -> coll_of(spec(is_integer()))
-        %{t: :string_array} -> coll_of(spec(is_binary()))
+        %{t: :list, coll_of: t, length: l} -> coll_of(get_t_spec(t, attr), min_count: l, max_count: l)
+        %{t: :list, coll_of: t} -> coll_of(get_t_spec(t, attr))
+        %{t: :integer} -> get_t_spec(:integer, attr)
+        %{t: :id} -> get_t_spec(:integer, attr)
+        %{t: :boolean} -> get_t_spec(:boolean, attr)
+        %{t: :string} -> get_t_spec(:string, attr)
+        %{t: :float} -> get_t_spec(:float, attr)
       end
       Map.put(acc, k, v)
     end))
@@ -42,15 +39,25 @@ defmodule Sorcery.SpecDb.NormHelpers do
   end
 
 
-  defp get_t_spec(:id), do: spec(is_integer())
-  defp get_t_spec(:integer), do: spec(is_integer())
-  defp get_t_spec(:string), do: spec(is_binary())
-  defp get_t_spec(:binary), do: spec(is_binary())
-  defp get_t_spec(:float), do: spec(is_float())
-  defp get_t_spec(:bool), do: spec(is_boolean())
-  defp get_t_spec(:boolean), do: spec(is_boolean())
-  defp get_t_spec(:trinary), do: one_of([true, false, nil])
-  defp get_t_spec(:atom), do: spec(is_atom())
+  defp get_t_spec(:integer, %{min: min, max: max}), do: spec(is_integer() and fn i -> i in min..max end)
+  defp get_t_spec(:integer, %{min: min}), do: spec(is_integer() and fn i -> i >= min end)
+  defp get_t_spec(:integer, %{max: max}), do: spec(is_integer() and fn i -> i <= max end)
+  defp get_t_spec(:integer, _), do: spec(is_integer())
+
+  defp get_t_spec(:float, %{min: min, max: max}), do: spec(is_float() and fn i -> i >= min and i <= max end)
+  defp get_t_spec(:float, %{min: min}), do: spec(is_float() and fn i -> i >= min end)
+  defp get_t_spec(:float, %{max: max}), do: spec(is_float() and fn i -> i <= max end)
+  defp get_t_spec(:float, _), do: spec(is_float())
+
+  defp get_t_spec(:string, %{min: min, max: max}), do: spec(is_binary() and fn i -> String.length(i) in min..max end)
+  defp get_t_spec(:string, %{min: min}), do: spec(is_binary() and fn i -> String.length(i) >= min end)
+  defp get_t_spec(:string, %{max: max}), do: spec(is_binary() and fn i -> String.length(i) <= max end)
+  defp get_t_spec(:string, _), do: spec(is_binary())
+
+
+  defp get_t_spec(:boolean, _), do: spec(is_boolean())
+  defp get_t_spec(:trinary, _), do: one_of([true, false, nil])
+  defp get_t_spec(:atom, _), do: spec(is_atom())
 
 
 end
