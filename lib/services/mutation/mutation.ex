cdef18839d437c8e059c37b873114649676f7bfb
Aaron <coding.aaronp@gmail.com>
2024-04-27 06:58:38 -0700

    Update lib version

--- lib/services/mutation/mutation.ex
+++ lib/services/mutation/mutation.ex
@@ -1,12 +1,24 @@
 defmodule Sorcery.Mutation do
+  import Sorcery.Helpers.Maps
+
   defstruct [
-    entities: %{},
-    args: %{}
+    version: 1,
+    args: %{},
+    frozen_data: %{},
+    inserts: %{},
+    deletes: %{},
+    updates: %{}
   ]
 
   def new(%{portal: _} = body) do
     body = Map.put_new(body, :args, %{})
     struct(__MODULE__, body)
   end
-  
+
+ 
+  def get(mutation, tk, id) do
+    frozen  = get_in_p(mutation, [:frozen_data, tk, id]) || %{}
+    updates = get_in_p(mutation, [:updates, tk, id])     || %{}
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sorcery.MixProject do
   def project do
     [
       app: :sorcery,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
