ed05b02f2e36feed63499c674363d1426d58d335
Aaron <coding.aaronp@gmail.com>
2024-05-04 07:33:04 -0700

    spawn_portals is now a setup function

--- test/services/portal_server/portal_test.exs
+++ test/services/portal_server/portal_test.exs
@@ -3,40 +3,11 @@ defmodule Sorcery.PortalServer.PortalTest do
   alias Sorcery.PortalServer.Portal, as: Portal
   doctest Portal
   alias Sorcery.ReturnedEntities, as: RE
+  import Sorcery.Setups
 
+  setup [:spawn_portal]
 
-  test "Portals" do
-    matches = %{
-      lvar_tks: %{
-        "?all_players" => :player,
-        "?arena" => :battle_arena,
-        "?player" => :player,
-      }, 
-      data: %{
-        "?all_players" => %{
-          1 => %{
-            id: 1,
-            name: "Nameless",
-            team_id: 1,
-            health: 31,
-            age: 70,
-            mana: 202,
-            money: 9271
-          },
-          2 => %{
-            id: 2,
-            name: "报",
-            team_id: 1,
-            health: 22,
-            age: 73,
-            mana: 341,
-            money: 5941
-          },
-        },
-        "?arena" => %{1 => %{id: 1, name: "Ice Room"}},
-        "?player" => %{1 => %{id: 1, team_id: 1}},
-      }}
-    portal = Portal.new(%{known_matches: matches})
+  test "Portals", %{portal: portal} do
     frozen_portal = Portal.freeze(portal)
     assert portal.known_matches.data["?arena"] == frozen_portal.known_matches.data.battle_arena
 
---
+++ test/support/setups.ex
@@ -0,0 +1,22 @@
+defmodule Sorcery.Setups do
+  use ExUnit.Case, async: true
+
+
+
+  # {{{ :spawn_portal
+  def spawn_portal(ctx) do
+    {:ok, pid} = GenServer.start_link(MyApp.PortalServers.Postgres, %{})
+    msg = %{
+      command: :spawn_portal,
+      from: self(),
+      args: %{player_id: 1, portal_name: :battle_portal},
+      query: MyApp.Queries.GetBattle,
+    }
+    send(pid, {:sorcery, msg})
+    assert_receive {:sorcery, %{args: %{portal: portal}}}
+    {:ok, %{portal: portal}}
+  end
+  # }}}
+
+
+end
